a9ec58b629fac3352ed5ce61de6c17d1dc16696d
gpuslave <alenemt@yandex.ru>
2024-08-13 12:05:18 +0300

    unique factorization kata

---
+++ py.py
@@ -0,0 +1,23 @@
+
+
+# x = [1, 2]
+# print(dir(x))
+# for i in x[:]:
+#     i += 1
+# print(x)
+
+# print('{0} {0} aboba'.format(1))
+
+# print(list(zip(['name', 'age'], ['John', 25])))
+
+# a = dict(zip((1, 2),
+#              ('age', 25)))
+# a = defaultdict(int)
+# print(a)
+# print(a.)
+# print(a[1])
+
+a = {1: 3, 2: 4}
+b = iter(a)
+print(next(b))
+print(next(b))
